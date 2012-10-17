4c1a62b8dc9acca6a7613d910515bb3559f5cdc6
Brett Jones <brettjonesdev@gmail.com>
2012-10-17 17:51:55 -0500

    Ignoring crap

--- bin/public/js/main/models/candidate.collection.js
+++
@@ -1,6 +0,0 @@
-define( ['backbone', 'underscore', 'jquery','models/candidate.model'], function(Backbone, _, $, Model) {
-	return Backbone.Collection.extend( {
-		model: Model,
-		url: '/candidates'
-	});
-});
